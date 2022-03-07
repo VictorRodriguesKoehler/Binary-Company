c8f8d99c497da33bdffe682f63d2484d9d1d6829
Victor Koehler <98185731+VictorRodriguesKoehler@users.noreply.github.com>
2022-03-07 11:54:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ $(function(){
         }
     })
 })
+
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+      setTimeout(() => elemento.innerHTML += letra, 75 * i);
+    });
+  }
+
+  const titulo = document.querySelector('.text-2');
+  typeWriter(titulo);
+
+//   const cards = document.querySelectorAll('.card')
+//   window.addEventListener('scroll', checkBoxes)
+
+//   checkBoxes()
+
+//   function checkBoxes(){
+//       const triggerBtn = window.innerHeight /5 * 4
+
+//       cards.forEach(box =>{
+//           const cardTop = card.getBoundingClientRect().Top
+
+//           if (boxTop < triggerBtn){
+//               box.classList.add('show')
+//           }else{
+//               box.classList.remove('show')
+//           }
+//       })
+//   }
